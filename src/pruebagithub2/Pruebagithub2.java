e712fdf06ba554cb52c3b9a89ab9c8645e54e6bf
sorona3 <nancysorondo@gmail.com>
2023-08-08 16:00:41 -0300

    Initial commit

---
+++ src/pruebagithub2/Pruebagithub2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagithub2;
+
+/**
+ *
+ * @author Sorondo
+ */
+public class Pruebagithub2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" este es mi prueba nancy");//para hacerme un repositorio y compartir
+    }
+    
+}
